04b227ab7eed2d0482f72081f2e1e216728c09a0
deepaksingh123021 <deepaksingh123021@gmail.com>
2019-07-20 14:14:50 +0530

    product page table fectch by database

--- dashboard/product.php
+++ dashboard/product.php
@@ -31,22 +31,24 @@
 			<th>Product Specification</th>
 			<th>Action</th>
 		</thead>
+<?php
+	$sql = "SELECT * FROM product";
+	$mysqli = new mysqli("localhost", "root", "", "galaxeep_shop");
+	$res = $mysqli->query($sql);
+
+	while ($rows = $res->fetch_array()) {
+?>
 		<tbody>
 			<tr>
-				<td>1</td>
-				<td>IPhone</td>
-				<td>IO10001</td>
-				<td>RS. 10,000 /-</td>
-				<td>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
+				<td><?php echo $rows[0] ?></td>
+				<td><?php echo $rows[1] ?></td>
+				<td><?php echo $rows[3] ?></td>
+				<td><?php echo $rows[2] ?></td>
+				<td><?php echo $rows[4] ?></td>
 				<td><a href="" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span></a></td>
 				<td><a href="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a></td>
 			</tr>
-			<tr>
+			<!-- <tr>
 				<td>2</td>
 				<td>IPhone</td>
 				<td>IO10001</td>
@@ -87,8 +89,9 @@
 				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</td>
 				<td><a href="prodUpdate.php?prodUID=1" class="btn btn-primary"><span class="glyphicon glyphicon-pencil"></span></a></td>
 				<td><a href="" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a></td>
-			</tr>
+			</tr> -->
 		</tbody>
+<?php } ?>
 	</table>
 </div>
 <?php require_once("include/footer.php"); ?>
